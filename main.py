09936678735b596938991c9cf2f31503c73d3be5
lanyanyemu <13538584284@163.com>
2022-09-13 15:24:13 +0800

    # 开始git,本地文件

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2022/9/13 15:20
+@Author  : jianwen.zhu
+@File    : test.py
+
+'''
+
+if __name__ == '__main__':
+    pass
+
---
+++ test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2022/9/13 15:20
+@Author  : jianwen.zhu
+@File    : test.py
+
+'''
